82544a92fd87bebf612a4112add14fdb44eca3fe
Hamza Iqbal <hi.hamza.dev@gmail.com>
2023-04-17 18:23:04 +0500

    custom hook http client

--- frontend/todo-list/src/hooks/httpClient.js
+++ frontend/todo-list/src/hooks/httpClient.js
@@ -1,12 +1,30 @@
 import { useCallback, useState } from "react";
+import axios from "axios";
 
 export const useHttpClient = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
 
-  const generateCall = useCallback((url, method, body, headers) => {
-    // get arguments and call API on URL by method attahcing body and headers
-  }, []);
+  const generateCall = useCallback(
+    async (url, method = "Get", body, headers) => {
+      setIsLoading(true);
+      setErrorMessage("");
+      try {
+        let res = await axios({
+          url,
+          method,
+          body,
+          headers,
+        });
+      } catch (err) {
+        console.log(err);
+        setIsLoading(false);
+        setErrorMessage(err);
+      }
+      // get arguments and call API on URL by method attahcing body and headers
+    },
+    []
+  );
 
   return { isLoading, errorMessage, generateCall };
 };
