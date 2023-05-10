bb8870e6e076703e87795f1268e59e068eb51c39
Hamza Iqbal <hi.hamza.dev@gmail.com>
2023-05-10 20:39:29 +0500

    adding custom hooks for optimizations

--- frontend/todo-list/src/hooks/httpClient.js
+++ frontend/todo-list/src/hooks/httpClient.js
@@ -1,31 +1,56 @@
-import { useCallback, useState } from "react";
-import axios from "axios";
-
-// take url, method-type, body, header and return response, loader and error state
+import { useState, useCallback, useRef, useEffect } from "react";
 
 export const useHttpClient = () => {
   const [isLoading, setIsLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState("");
+  const [error, setError] = useState();
 
-  const generateCall = useCallback(
-    async (url, method = "Get", body, headers) => {
+  const activeHttpRequests = useRef([]);
+
+  const sendRequest = useCallback(
+    async (url, method = "GET", body = null, headers = {}) => {
       setIsLoading(true);
-      setErrorMessage("");
+      const httpAbortCtrl = new AbortController();
+      activeHttpRequests.current.push(httpAbortCtrl);
+
       try {
-        let res = await axios({
-          url,
+        const response = await fetch(url, {
           method,
           body,
           headers,
+          signal: httpAbortCtrl.signal,
         });
-      } catch (err) {
-        console.log(err);
+
+        const responseData = await response.json();
+
+        activeHttpRequests.current = activeHttpRequests.current.filter(
+          (reqCtrl) => reqCtrl !== httpAbortCtrl
+        );
+
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
         setIsLoading(false);
-        setErrorMessage(err);
+        return responseData;
+      } catch (err) {
+        setError(err.message);
+        setIsLoading(false);
+        throw err;
       }
     },
     []
   );
 
-  return { isLoading, errorMessage, generateCall };
+  const clearError = () => {
+    setError(null);
+  };
+
+  useEffect(() => {
+    return () => {
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      activeHttpRequests.current.forEach((abortCtrl) => abortCtrl.abort());
+    };
+  }, []);
+
+  return { isLoading, error, sendRequest, clearError };
 };
