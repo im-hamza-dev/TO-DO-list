bfa63dc7a999cace16518a2ad2925b22fc11d896
im-hamza-dev <hi.hamza.dev@gmail.com>
2023-05-14 20:50:41 +0500

    services restructuring

--- frontend/todo-list/src/services/notesService.js
+++ frontend/todo-list/src/services/notesService.js
@@ -1,15 +1,15 @@
 import { post, get, put } from "../singelton/axios/index";
-import * as url from "../helpers/url_helper";
+import NotesAPI from "../utils/config";
 
 // get contacts
-export const getNotes = () => get(url.GET_CAMERAS);
+export const getNotes = () => get(NotesAPI.getNote);
 
-export const addNotes = (Notes) => post(url.ADD_Notes, Notes);
+export const addNotes = (Notes) => post(NotesAPI.createNote, Notes);
 
 export const updateNotes = (Notes) =>
-  put(`${url.UPDATE_Notes}/${Notes.id}`, Notes);
+  put(`${NotesAPI.updateNote}/${Notes.id}`, Notes);
 
-export const ActivateUpdateNotes = (Notes) =>
-  put(`${url.UPDATE_Notes}/${Notes.id}/activate`, Notes);
+export const deleteNote = (Notes) =>
+  put(`${NotesAPI.deleteNote}/${Notes.id}/activate`, Notes);
 
 // notes module services
